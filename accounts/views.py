6f5028fea492ac87149fff8081179094e13e0cca
Jay1425 <jayraychura13@gmail.com>
2026-07-12 12:35:54 +0530

    started frontend and authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,31 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import LoginForm
 
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
 
-    form = LoginForm(request, data=request.POST or None)
-
     if request.method == "POST":
-        if form.is_valid():
-            user = form.get_user()
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
             login(request, user)
+
+            # We'll add role-based redirect later
             return redirect("dashboard")
 
-    return render(request, "accounts/login.html", {"form": form})
+        messages.error(request, "Invalid username or password.")
+
+    return render(request, "accounts/login.html")
 
 
 def logout_view(request):
